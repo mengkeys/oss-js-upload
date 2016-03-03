1eed7c74e083aa1ccb57ed2456b4bbf799f64126
mengkeys <mengkeys@users.noreply.github.com>
2016-03-04 01:55:10 +0800

    变更配置为自己的
    变更配置为自己的

--- demoServer.js
+++ demoServer.js
@@ -4,20 +4,20 @@ var serveStatic = require('serve-static');
 var path = require('path');
 var ALY = require('aliyun-sdk');
 var sts = new ALY.STS({
-      accessKeyId: '在设置 RAM 时得到的 accessKeyId',
-      secretAccessKey: '在设置 RAM 时得到的 accessKeySecret',
+      accessKeyId: 'y3ioHAWIthAGx7cx',
+      secretAccessKey: 'rVKY2g75k1f9gUOQhJJvGv1Wl8nRPB',
       endpoint: 'https://sts.aliyuncs.com',
       apiVersion: '2015-04-01'
     }
 );
 // 必需要准备账号 ID, 可以在 https://account.console.aliyun.com/#/secure 找到
-var accountId = '账号 ID';
-var roleARN = '在设置 RAM 时得到的 roleARN';
+var accountId = '1263645526084457';
+var roleARN = 'acs:ram::1263645526084457:role/tech';
 // 授权访问的资源表达式
 // 如果允许用户操作整个 Bucket, 则为 bucket_name/*
 // 如果允许用户操作某个文件夹, 则为 bucket_name/path/to/folder/*
 // 如果仅允许用户上传到某个具体文件, 则为 bucket_name/path/to/folder/file
-var resource = '';
+var resource = 'dongda/*';
 
 if (!resource) {
   console.error('必须设置 resource');
@@ -31,17 +31,18 @@ app.get('/token', function (req, res) {
     RoleArn: roleARN,
     RoleSessionName: 'chylvina',
     Policy: '{"Version":"1","Statement":[{"Effect":"Allow","Action":["oss:PutObject","oss:CreateMultipartUpload","oss:UploadPart","oss:CompleteMultipartUpload"],"Resource":["acs:oss:*:' + accountId +':' + resource + '"]}]}',
-    DurationSeconds: 1000
+    DurationSeconds: 3000
   }, function (err, data) {
     if (err) {
-      return res.send(500, err);
+
+      return res.status(500).send(err);
     }
 
     res.json(data);
   });
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(3005, function () {
 
   var host = server.address().address;
   var port = server.address().port;
